86ab80d4e370acc5a274dde621edd580d29c21b3
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2025-09-26 21:16:17 +0900

    added new test

---
+++ crates/lib/mimium-lang/state-tree/tests/delay.rs
@@ -0,0 +1,55 @@
+use rkyv::{
+    DeserializeUnsized, access, access_mut, api::deserialize_using, deserialize, munge::munge,
+    to_bytes, util::AlignedVec,
+};
+use state_tree::tree::{ArchivedStateTree, StateTree};
+
+#[test]
+fn archived_delay_update() {
+    let tree = StateTree::Delay {
+        typesize: 1,
+        readidx: 0,
+        writeidx: 0,
+        data: vec![0, 0, 0, 0].into_boxed_slice(),
+    };
+    let mut new_bytes =
+        to_bytes::<rkyv::rancor::Error>(&tree).expect("Failed to serialize new_tree");
+    let archived_new = access_mut::<ArchivedStateTree, rkyv::rancor::Error>(&mut new_bytes)
+        .expect("Failed to access archived_new");
+    let hoge = unsafe { archived_new.unseal_unchecked() };
+    match hoge {
+        ArchivedStateTree::Delay {
+            readidx, writeidx, ..
+        } => {
+            *readidx = (u64::from(*readidx) + 1u64).into();
+            *writeidx = (u64::from(*writeidx) + 1u64).into();
+        }
+        _=> panic!(),
+    };
+    let readidx = *match hoge {
+        ArchivedStateTree::Delay { readidx, .. } => readidx,
+        _ => panic!(),
+    };
+    let writeidx = *match hoge {
+        ArchivedStateTree::Delay { writeidx, .. } => writeidx,
+        _ => panic!(),
+    };
+    assert_eq!(readidx, 1);
+    assert_eq!(writeidx, 1);
+    //read again from same memory and check values is correctly updated
+    let archived_new = unsafe {
+        access_mut::<ArchivedStateTree, rkyv::rancor::Error>(&mut new_bytes)
+            .expect("Failed to access archived_new")
+            .unseal_unchecked()
+    };
+    let readidx = *match archived_new {
+        ArchivedStateTree::Delay { readidx, .. } => readidx,
+        _ => panic!(),
+    };
+    let writeidx = *match archived_new {
+        ArchivedStateTree::Delay { writeidx, .. } => writeidx,
+        _ => panic!(),
+    };
+    assert_eq!(readidx, 1);
+    assert_eq!(writeidx, 1);
+}
