1282933d4088fb1a1afe48079a22688f43e767fb
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2025-09-27 16:12:09 +0900

    Revert "Continue investigating staging issue - tried Unknown type annotation"
    This reverts commit 34f27252c7a738bc3d87d571a23cf12d43d23033.

--- crates/lib/plugins/mimium-guitools/src/lib.rs
+++ crates/lib/plugins/mimium-guitools/src/lib.rs
@@ -101,7 +101,7 @@ impl GuiToolPlugin {
             Expr::Lambda(
                 vec![TypedId::new(
                     "x".to_symbol(),
-                    Type::Unknown.into_id(),
+                    Type::Primitive(PType::Numeric).into_id(),
                 )],
                 None,
                 Expr::Apply(
