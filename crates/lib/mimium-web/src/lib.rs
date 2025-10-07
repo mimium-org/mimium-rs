dbf1a5c411e941d49b661ba48ff29a55ab8e6813
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2025-10-07 14:01:44 +0900

    updated web api

--- crates/lib/mimium-web/src/lib.rs
+++ crates/lib/mimium-web/src/lib.rs
@@ -1,10 +1,11 @@
 use std::path::PathBuf;
+use std::sync::mpsc;
 
 use mimium_audiodriver::backends::local_buffer::LocalBufferDriver;
 use mimium_audiodriver::driver::{Driver, RuntimeData};
 use mimium_lang::ExecContext;
-use mimium_lang::interner::ToSymbol;
 use mimium_lang::log;
+use mimium_lang::runtime::vm;
 use mimium_lang::utils::error::report;
 use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
@@ -27,10 +28,12 @@ type Output<'a> = &'a mut [f32];
 type Input<'a> = &'a [f32];
 
 type Processer = Box<dyn FnMut(Input, Output) -> u64>;
+
 #[wasm_bindgen]
 #[derive(Default)]
 pub struct Context {
     processor: Option<Processer>,
+    swap_channel: Option<mpsc::Sender<vm::Program>>,
     config: Config,
 }
 
@@ -57,6 +60,8 @@ impl Context {
     }
     #[wasm_bindgen]
     pub fn compile(&mut self, src: String) {
+        let (sender, receiver) = mpsc::channel();
+        self.swap_channel = Some(sender);
         let mut ctx = get_default_context();
         let mut driver = LocalBufferDriver::new(self.config.buffer_size as usize);
         ctx.add_plugin(driver.get_as_plugin());
@@ -81,6 +86,9 @@ impl Context {
         let out_ch = self.config.output_channels;
         let mut out_buf = vec![0.0; (out_ch * self.config.buffer_size) as usize];
         self.processor = Some(Box::new(move |_input, output: Output| -> u64 {
+            if let (Some(vmdata), Ok(prog)) = (driver.vmdata.as_mut(), receiver.try_recv()) {
+                vmdata.vm = vmdata.vm.new_resume(prog);
+            };
             driver.play();
             driver
                 .get_generated_samples()
@@ -93,6 +101,25 @@ impl Context {
             output.copy_from_slice(&out_buf);
             0
         }));
+    }
+    #[wasm_bindgen]
+    pub fn recompile(&mut self,src:String){
+        let mut ctx = get_default_context();
+        let driver = LocalBufferDriver::new(self.config.buffer_size as usize);
+        ctx.add_plugin(driver.get_as_plugin());
+        match ctx.prepare_machine(src.as_str()) {
+            Err(e) => {
+                report(&src, PathBuf::from("/"), &e);
+            }
+            Ok(()) => {
+                ctx.run_main();
+                let prog = ctx.take_vm().unwrap().prog;                
+                self.config.input_channels = prog.iochannels.map_or(0, |io| io.input);
+                self.config.output_channels = prog.iochannels.map_or(0, |io| io.output);
+                self.swap_channel.as_mut().unwrap().send(prog).unwrap();
+            }
+            
+        }
     }
     #[wasm_bindgen]
     pub fn get_input_channels(&self) -> u32 {
